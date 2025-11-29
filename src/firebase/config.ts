209c50738d698a02ff93abb83a5adf93f72194e4
Джони Кабаев <abaevevgenchik@gmail.com>
2025-11-29 22:17:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1015620650-16b63",
+  "appId": "1:144438969364:web:79ffc09772dd19660a1301",
+  "apiKey": "AIzaSyC9Nnp-52_P6Hkv7VHJLqJ9SdnUv-4luvg",
+  "authDomain": "studio-1015620650-16b63.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "144438969364"
+};
