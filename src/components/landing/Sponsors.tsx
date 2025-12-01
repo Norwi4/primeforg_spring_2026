63c344264f8f2b9fe23f2ef4d10f1df041f9b357
Джони Кабаев <abaevevgenchik@gmail.com>
2025-12-01 06:43:34 +0000

    во фразе "Мы благодарны нашим спонсорам, которые делают этот турнир возм

--- src/components/landing/Sponsors.tsx
+++ src/components/landing/Sponsors.tsx
@@ -46,7 +46,7 @@ export default function Sponsors() {
               Партнеры турнира
             </h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Мы благодарны нашим спонсорам, которые делают этот турнир возможным.
+              Мы благодарны нашим партнерам, которые делают этот турнир возможным.
             </p>
           </div>
         </div>
